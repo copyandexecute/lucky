dceb0cfc2e75076fbed040bec606ff98a8c76679
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2023-03-09 21:00:24 +0100

    added start/stop command

--- src/main/kotlin/de/hglabor/lucky/Lucky.kt
+++ src/main/kotlin/de/hglabor/lucky/Lucky.kt
@@ -1,25 +1,66 @@
 package de.hglabor.lucky
 
 import de.hglabor.lucky.mixin.loot.accessor.LeafEntryAccessor
+import kotlinx.coroutines.Job
 import net.fabricmc.api.ModInitializer
 import net.minecraft.enchantment.EnchantmentHelper
 import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.attribute.EntityAttributes
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.loot.context.LootContext
 import net.minecraft.loot.provider.number.LootNumberProvider
+import net.minecraft.server.MinecraftServer
+import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.util.math.MathHelper
 import net.silkmc.silk.commands.command
 import net.silkmc.silk.core.logging.logInfo
+import net.silkmc.silk.core.task.infiniteMcCoroutineTask
+import net.silkmc.silk.core.text.literalText
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
 
 object Lucky : ModInitializer {
     private var isActive: Boolean = false
+    private var jobs: MutableList<Job> = mutableListOf()
+    private const val LUCK_COLOR = 0x00ff26
+    private val MinecraftServer.players: MutableList<ServerPlayerEntity>
+        get() = playerManager.playerList
+
 
     override fun onInitialize() {
         logInfo("Initializing Lucky Mod")
 
         command("lucky") {
             literal("start") {
-                
+                runs {
+                    this.source.sendMessage(literalText("Viel Glück ;)") {
+                        color = LUCK_COLOR
+                    })
+                    isActive = true
+                    val server = this.source.server
+                    jobs += infiniteMcCoroutineTask(period = 1.minutes) {
+                        server.players.forEach {
+                            val luckInstance = it.attributes.getCustomInstance(EntityAttributes.GENERIC_LUCK) ?: return@forEach
+                            luckInstance.baseValue += 15.0
+                        }
+                    }
+                    jobs += infiniteMcCoroutineTask(sync = false) {
+                        server.players.forEach {
+                            it.sendMessage(literalText("Glück ${it.luck}") {
+                                color = LUCK_COLOR
+                            }, true)
+                        }
+                    }
+                }
+            }
+            literal("stop") {
+                runs {
+                    this.source.sendMessage(literalText("Nicht mehr viel Glück ;)") {
+                        color = 0xa200ff
+                    })
+                    jobs.forEach(Job::cancel)
+                    isActive = false
+                }
             }
         }
     }
